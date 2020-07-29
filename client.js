ceb8410024973a898765e46595bbe5e9a4c7f40e
Scott Grunerud <sgrunerud@edu.uwaterloo.ca>
2020-07-29 15:25:18 +0000

    Adding ability to name each snake

--- client.js
+++ client.js
@@ -7,6 +7,14 @@ const connect = () => {
   });
   conn.setEncoding("utf-8");
 
+  conn.on('connect', (data) => {
+    console.log('Connection Established');
+    conn.write('Name: SAG');
+
+  });
+
+  
+
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
