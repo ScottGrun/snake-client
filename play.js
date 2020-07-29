9a71c46d47578b234111abc51610d081af6f9c8c
Scott Grunerud <sgrunerud@edu.uwaterloo.ca>
2020-07-29 16:31:10 +0000

    Splitting up files

--- play.js
+++ play.js
@@ -1,25 +1,10 @@
 const connect = require("./client");
+const { setupInput } = require('./input');
 
-const setupInput = function (callback) {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", (key) => {
-    console.log("hit");
-    callback(key);
-  });
 
-  return stdin;
-};
 
-const handleUserInput = (key) => {
-  if (key === "\u0003") {
-    process.exit();
-  }
-};
-
-setupInput(handleUserInput);
 
 console.log("connecting");
 connect();
+setupInput();
+
